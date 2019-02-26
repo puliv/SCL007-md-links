707d1f3498d5f31ab104fa41a9e3c98bca7f2c93
puliv <pverapavez@gmail.com>
2019-02-26 17:56:49 -0300

    Extraer links, listo

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-module.exports = () => {
-  // ...
+// module.exports = () => {
+//   // ...
+// };
+
+
+/*Use readFile para leer README.md y me mostro su contenido, por que use el parametro 
+utf-8 para que use ese codigo al mostrarlo*/
+
+// const path = require("path");
+
+// const basenamePath= path.basename("README.md")
+// 	console.log(basenamePath)
+
+// const extensionPath = path.extname("README.md")
+//     console.log(extensionPath)
+
+// const readline = require("readline");
+
+
+const fs = require("fs");
+const anyDocument = process.argv[2];
+const fetch = require("node-fetch");
+let dataLinks = []; 
+
+
+const mdLinks = () => {
+	//Modulo que captura los links 
+	const markdownLinkExtractor = require('markdown-link-extractor');
+
+	fs.readFile(anyDocument, "utf-8", (err, data) => {
+		if (err) {
+			console.log(err)
+		} else {
+			const links = markdownLinkExtractor(data);
+				console.log(links);
+				dataLinks.push(links); //Aqui guardo el resultado para usarlo con fetch
+		}
+	});
 };
+
+// mdLinks();
+
+fetch("README.md").then((res) => {	
+	console.log(res)
+}); 
+
