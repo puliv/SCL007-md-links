1579271c1fdeea8fce412a1ae36e7c739f0cc9ff
puliv <pverapavez@gmail.com>
2019-02-27 16:47:56 -0300

    links verificados e impresos en terminal como arr de obj

--- index.js
+++ index.js
@@ -9,24 +9,25 @@ const fetch = require("node-fetch");
 const mdLinks = () => {
 	//Modulo que captura los links 
 	const markdownLinkExtractor = require('markdown-link-extractor');
-
+	
 	fs.readFile(anyDocument, "utf-8", (err, data) => {
 		if (err) {
 			console.log(err)
 		} else {
 			const links = markdownLinkExtractor(data);
 			// console.log(links);
-
+			
+			const arrFetch = [];
 			for (let i = 0; i < links.length; i++) {
 				const linksExtracted = links[i];
 				
-				fetch(links[i]).then(res => {
+				const fetchLinks = fetch(links[i]).then(res => {
 					const objectLinks = {
 						url: res.url,
 						statusLinks: res.status,
 						statusText: res.statusText
 					};
-					console.log(objectLinks);
+					// console.log(objectLinks);
 					return objectLinks;
 				})
 				.catch(error => {
@@ -34,12 +35,16 @@ const mdLinks = () => {
 						urlErr: linksExtracted,
 						statusErr: error
 					}
-					console.log(objectErr)
+					// console.log(objectErr)
 					return objectErr;
 				});
+				arrFetch.push(fetchLinks);
 			}
+			Promise.all (arrFetch)
+			.then((res)=> console.log (res));
 		}
 	});
+
 };
 
 mdLinks();
