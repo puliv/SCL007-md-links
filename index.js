9d23b6f84d63c6961aa949175d3cdfd44314601e
puliv <pverapavez@gmail.com>
2019-03-01 18:04:08 -0300

    se puede agregar la opcion --validate para ver si los links estan buenos o no

--- index.js
+++ index.js
@@ -2,21 +2,18 @@
 //   // ...
 // };
 
-// module.exports = () => {
-//   // ...
-// };
-
 const fs = require("fs");
 const path = process.argv[2];
 const fetch = require("node-fetch");
 const pathModule = require("path");
 const markdownLinkExtractor = require('markdown-link-extractor'); //Modulo que captura los links 
+const options = process.argv[3];
 
 const mdLinks = (path, option) => {
 
 	const stat = fs.statSync(path);
-	const absolutePath = pathModule.resolve(path); 
-	const fileExtension = pathModule.extname(path); 
+	const absolutePath = pathModule.resolve(path);
+	const fileExtension = pathModule.extname(path);
 
 
 	if (fileExtension === ".md") {
@@ -30,22 +27,34 @@ const mdLinks = (path, option) => {
 				const arrFetch = []; //Recorro los links para pasarlos al fetch y que guarde los objetos dentro de este arr
 				for (let i = 0; i < links.length; i++) {
 					const linksExtracted = links[i];
+					const text = links[i].text;
 
 					//con el Fetch le pido que me muestre como objeto datos especificos
 					const fetchLinks = fetch(links[i]).then(res => {
-						const objectLinks = {
-							url: res.url,
-							statusLinks: res.status,
-							statusText: res.statusText,
-							path: absolutePath
-						};
-						// console.log(objectLinks);
-						return objectLinks;
+						if (options === "--validate") {
+							const objectLinks = {
+								url: res.url,
+								path: absolutePath,
+								text: text,
+								statusLinks: res.status,
+								statusText: res.statusText
+							}
+							return objectLinks
+
+						} else {
+							const objectLink = {
+								href: res.url,
+								text: text,
+								path: absolutePath
+							}
+							return objectLink;
+						}
 					})
 						.catch(error => {
 							const objectErr = {
-								urlErr: linksExtracted,
-								statusErr: error
+								url: linksExtracted,
+								statusLinks: "error",
+								text: text
 							}
 							// console.log(objectErr)
 							return objectErr;
@@ -64,7 +73,7 @@ const mdLinks = (path, option) => {
 			return mdLinks(otherVar);
 		})))
 	} else {
-		console.log("Errorrrrrrrrrrrr")
+		console.log("You should try with a .md file")
 	}
 };
 
